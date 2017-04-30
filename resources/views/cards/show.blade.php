50d783ed998651368bda47634f4524d0d7f14e92
Luke Hardman <u1465774@hud.ac.uk>
2017-04-30 23:54:03 +0100

    update to the cards page
    the user can now create new cards

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -18,7 +18,15 @@
       <hr>
       <h3> Add a new note about the car</h3>
 
-        
+        <form method="POST" action="/cards/{{ $card->id }}/notes">
+          <div class="form-group">
+            <textarea name="body" class="form-control"></textarea>
+          </div>
+
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary">Submit Note</button>
+          </div>
+        </form>
       </div>
     </div>
 @stop
