4a83913d775d4586dff29d48ca945871f147cecf
Luke Hardman <u1465774@hud.ac.uk>
2017-05-02 13:02:00 +0100

    Updating the main view

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -11,7 +11,9 @@
 
       <ul class="list-group">
         @foreach ($card->notes as $note)
-          <li class="list-group-item">{{ $note->body }}</li>
+          <li class="list-group-item">{{ $note->body }}
+            <a href="#" style="float:right"> {{ $note->user->username }}</a>
+          </li>
         @endforeach
       </ul>
 
@@ -19,14 +21,27 @@
       <h3> Add a new note about the car</h3>
 
         <form method="POST" action="/cards/{{ $card->id }}/notes">
+          {{ csrf_field() }}
           <div class="form-group">
-            <textarea name="body" class="form-control"></textarea>
+            <textarea name="body" class="form-control">{{ old('body') }}</textarea>
           </div>
 
           <div class="form-group">
             <button type="submit" class="btn btn-primary">Submit Note</button>
           </div>
         </form>
+
+        @if (count($errors))
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li style="color:red">
+              {{ $error }}
+            </li>
+          @endforeach
+        </ul>
+      @endif
+
+
       </div>
     </div>
 @stop
