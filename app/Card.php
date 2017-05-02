f7a663a7df7a9094d9b2cbe29679809cc8888482
Luke Hardman <u1465774@hud.ac.uk>
2017-05-02 01:19:29 +0100

    Adding card and note classes
    linking both the cards and notes together

---
+++ app/Card.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    public function notes()
+    {
+      return $this->hasMany(Note::class);
+    }
+
+    public function addNote(Note $note)
+    {
+      return $this->notes()->save($note);
+    }
+
+}
